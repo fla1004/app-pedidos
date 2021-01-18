b55cb040a71db50998057f351dd3afd1c51b4a10
fla1004 <fla1004@users.noreply.github.com>
2021-01-18 11:35:26 -0400

    jwt

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,6 @@
 module.exports = {
     port : 3000,
+    JWT_SECRET: "MI_CODIGO_SECRETO",
+    JWT_EXPIRE: '1h'
 }
+
--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,9 @@
 const Usuario = require("./../models/Usuario");
+const config = require("./../config/config")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
 
 const login = async function login(req,res)
 {
@@ -11,7 +16,34 @@ const login = async function login(req,res)
     }
     else
     {
-        res.json({mensaje: `Bienvenido: ${req.body.correo}`});
+        //Compara las contraseñas
+        const estado = await bcrypt.compare(req.body.password, user.password)
+        if(estado){
+            //Generar token
+            const payload = {
+                correo: user.correo,
+                id: user._id,
+                time: new Date()
+            }
+
+            var token = jwt.sign(payload,config.JWT_SECRET, {
+                expiresIn: config.JWT_EXPIRE,
+            });
+
+            res.json({
+                //access_token:token,
+                //Usuario: {
+                  //  _id: user._id,
+                    usuario: user.usuario,
+                    correo: user.correo,
+                    fecha: new Date(),
+                //}
+            });
+        }
+        else{
+            res.json({mensaje:"Contraseña incorrecta"});
+        }
+        
     }
 }
 module.exports = {
